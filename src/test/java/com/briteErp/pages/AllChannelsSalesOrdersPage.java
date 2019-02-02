444033103196a05444595f08cff157cb517cdd91
sasori07 <sasori07@hotmail.com>
2019-02-01 22:54:38 -0500

    cihan's

--- src/test/java/com/briteErp/pages/AllChannelsSalesOrdersPage.java
+++ src/test/java/com/briteErp/pages/AllChannelsSalesOrdersPage.java
@@ -1,11 +1,54 @@
 package com.briteErp.pages;
 
+import com.briteErp.utilities.ConfigurationReader;
 import com.briteErp.utilities.Driver;
+import com.briteErp.utilities.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
-public class AllChannelsSalesOrdersPage {
+import static com.briteErp.utilities.BrowserUtils.waitForPageToLoad;
+
+public class AllChannelsSalesOrdersPage  extends TestBase {
     public AllChannelsSalesOrdersPage() {
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
+
+    @FindBy(linkText = "Sales")
+    public WebElement salesLink;
+
+    @FindBy(linkText = "SalesChannel")
+    public WebElement salesChannel;
+
+    @FindBy(xpath = "//button[contains(text(),'Create')]")
+    public WebElement createButton;
+
+    @FindBy(xpath = "//input[@id='o_field_input_2871']")
+    public WebElement salesChannelBox ;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/button[1]")
+    public WebElement saveButton ;
+
+
+    @FindBy (linkText = "active")
+    public WebElement activeLink ;
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/briteErp/tests/functional_tests/salesCreateTest.java
+++ src/test/java/com/briteErp/tests/functional_tests/salesCreateTest.java
@@ -1,4 +1,78 @@
 package com.briteErp.tests.functional_tests;
 
-public class salesCreateTest {
+import com.briteErp.utilities.ConfigurationReader;
+import com.briteErp.utilities.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.briteErp.utilities.BrowserUtils.waitForPageToLoad;
+
+public class salesCreateTest extends TestBase {
+
+    @Test
+    public void salesCreate() {
+
+        extentLogger = report.createTest("Sales Create Options");
+        extentLogger.info("Verifying create option is available.");
+
+        pages.login().BriteErpLink.click();
+
+        extentLogger.info("Login to application");
+        pages.login().login(ConfigurationReader.getProperty("emailUser"), ConfigurationReader.getProperty("passwordUser"));
+
+        pages.allChannelsSalesOrdersPage().salesLink.click();
+        waitForPageToLoad(10000);
+
+        pages.allChannelsSalesOrdersPage().salesChannel.click();
+        waitForPageToLoad(10000);
+
+        pages.allChannelsSalesOrdersPage().createButton.click();
+        waitForPageToLoad(10000);
+
+        pages.allChannelsSalesOrdersPage().salesChannelBox.sendKeys("JapanX");
+        waitForPageToLoad(10000);
+
+        pages.allChannelsSalesOrdersPage().saveButton.click();
+
+        extentLogger.info("Verifying create link is available");
+        Assert.assertEquals(pages.allChannelsSalesOrdersPage().salesLink.getText(), "create");
+
+    }
+
+        @Test
+        public void salesStatusTest() {
+
+            extentLogger = report.createTest("Sales Create Options");
+            extentLogger.info("Verifying create option is available.");
+
+            pages.login().BriteErpLink.click();
+
+            extentLogger.info("Login to application");
+            pages.login().login(ConfigurationReader.getProperty("emailUser"), ConfigurationReader.getProperty("passwordUser"));
+
+            pages.allChannelsSalesOrdersPage().salesLink.click();
+            waitForPageToLoad(10000);
+
+            pages.allChannelsSalesOrdersPage().salesChannel.click();
+            waitForPageToLoad(10000);
+
+            pages.allChannelsSalesOrdersPage().createButton.click();
+            waitForPageToLoad(10000);
+
+            pages.allChannelsSalesOrdersPage().salesChannelBox.sendKeys("JapanX");
+            waitForPageToLoad(10000);
+
+            pages.allChannelsSalesOrdersPage().activeLink.click();
+
+            extentLogger.info("Clicking on 'Active' link ");
+            extentLogger.info("Verifying user  able to change the product status ");
+
+
+
+
+
+
+
+
+    }
 }
