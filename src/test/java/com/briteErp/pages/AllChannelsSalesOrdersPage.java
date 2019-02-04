e384a2c3f7bf8d62b46471d334489bbe4f9ce41c
Osman Sahin <37234586+osmanshn@users.noreply.github.com>
2019-02-03 21:02:49 -0600

    All Channels Sales Orders Page

--- src/test/java/com/briteErp/pages/AllChannelsSalesOrdersPage.java
+++ src/test/java/com/briteErp/pages/AllChannelsSalesOrdersPage.java
@@ -1,39 +1,87 @@
 package com.briteErp.pages;
 
-import com.briteErp.utilities.ConfigurationReader;
 import com.briteErp.utilities.Driver;
-import com.briteErp.utilities.TestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.Select;
 
-import static com.briteErp.utilities.BrowserUtils.waitForPageToLoad;
-
-public class AllChannelsSalesOrdersPage  extends TestBase {
+public class AllChannelsSalesOrdersPage {
     public AllChannelsSalesOrdersPage() {
-        PageFactory.initElements(Driver.getDriver(), this);
+        PageFactory.initElements(Driver.getDriver(),this);
     }
 
-
     @FindBy(linkText = "Sales")
-    public WebElement salesLink;
+    public WebElement salesMenu;
 
-    @FindBy(linkText = "SalesChannel")
+    @FindBy (partialLinkText = "All Channels Sales Orders")
+    public WebElement allChannelsSalesOrdersLink;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/button")
+    public WebElement measuresMenu;
+
+    @FindBy (linkText = "Price Subtotal")
+    public WebElement priceSubtotal;
+
+    @FindBy (linkText = "Total")
+    public WebElement total;
+
+    @FindBy (linkText = "Product Quantity")
+    public WebElement productQuantity;
+
+    @FindBy (linkText = "Count")
+    public WebElement count;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/button[1]")
+    public WebElement flipAxis;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/button[2]")
+    public WebElement expandAll;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/button[3]")
+    public WebElement download;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[1]/div/span")
+    public WebElement advancedSearch;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/button")
+    public WebElement filters;
+
+    @FindBy (linkText = "Current Year Sales")
+    public WebElement currentYearSales;
+
+    @FindBy (linkText = "Add Custom Filter")
+    public WebElement addCustomFilter;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]/button")
+    public WebElement groupBy;
+
+    @FindBy (linkText = "Sales Channel")
     public WebElement salesChannel;
 
-    @FindBy(xpath = "//button[contains(text(),'Create')]")
-    public WebElement createButton;
+    @FindBy (linkText = "Add custom group")
+    public WebElement addCustomGroup;
 
-    @FindBy(xpath = "//input[@id='o_field_input_2871']")
-    public WebElement salesChannelBox ;
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/div[3]/div[1]/div[3]/button")
+    public WebElement favorites;
 
-    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/button[1]")
-    public WebElement saveButton ;
+    @FindBy (linkText = "Save current search")
+    public WebElement saveCurrentSearch;
+
+    @FindBy (linkText = "Add to my Dashboard")
+    public WebElement addToMyDashboard;
+
+    @FindBy (xpath = "/html/body/div[1]/div[2]/div[1]/ol/li")
+    public WebElement allChannelsSalesOrdersTitle;
+
+    @FindBy (xpath = "//li[@class='o_save_name']/input[1]")
+    public WebElement favoritesText;
+
+    @FindBy (xpath = "//button[@class='btn btn-primary btn-sm']")
+    public WebElement favoritesSaveButton;
 
 
-    @FindBy (linkText = "active")
-    public WebElement activeLink ;
 
 
 
@@ -41,14 +89,3 @@ public class AllChannelsSalesOrdersPage  extends TestBase {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
